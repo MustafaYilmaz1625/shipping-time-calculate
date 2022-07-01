0f7f94a8e42f18ecc76e019fbd6fd7f57f6492d6
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-07-01 17:59:30 +0300

    built in CSS

--- components/UserList.js
+++ components/UserList.js
@@ -2,12 +2,22 @@ import Link from "next/link";
 const UserList = () => (
   <div>
     <Link href="/user?name=mehmet" as="mehmet">
-      <a>Mehmet</a>
+      <a className="username">Mehmet</a>
     </Link>
     <br />
     <Link href="/user?name=ahmet" as="ahmet">
-      <a>Ahmet</a>
+      <a className="username">Ahmet</a>
     </Link>
+
+    <style jsx>{`
+      .username {
+        background: green;
+        color: white;
+        font-size: 20px;
+        font-weight: bold;
+        margin-left: 10px;
+      }
+    `}</style>
   </div>
 );
 
