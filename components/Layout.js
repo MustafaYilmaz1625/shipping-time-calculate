984774d0137402f3c48af42f7558ea807e547248
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-07-01 18:00:07 +0300

    Head component-next.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,15 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 const Layout = (props) => (
   <div>
+    <Head>
+      <title>My Website</title>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/css/bootstrap.min.css"
+      />
+    </Head>
     <Navbar />
     {props.children}
   </div>
