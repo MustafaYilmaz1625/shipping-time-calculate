bd3f6ce2f2f8af64bcb9b4135c5e723a880e5aa8
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-10-28 23:41:25 +0300

    add calculate folder on page.

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-
-const Home = () => (
-  <Layout>
-    <div>Home page</div>
-  </Layout>
-);
-
-export default Home;
--- pages/user.js
+++
@@ -1,9 +0,0 @@
-const User = (props) => (
-  <div>
-    <h2>User Detail</h2>
-    <br />
-    {props.url.query.name}
-  </div>
-);
-
-export default User;
