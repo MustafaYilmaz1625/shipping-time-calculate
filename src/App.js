9566dd6c8791ba5af78df13ed8fa5f835ab85683
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-10-30 04:01:44 +0300

    add update on page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,161 @@
-import './App.css';
-import Calculator from './components/Calculate';
+import React, { useEffect, useState } from "react";
+import Calculate from "./components/calculate/Calculate";
+import { BsCalendar4 } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
+import { TiWarningOutline } from "react-icons/ti";
+import "./App.css";
 
 function App() {
+  const [type, setType] = useState("text");
+  const initialValues = { date: "", fabric: "", quantity: "" };
+  const [values, setValues] = useState(initialValues);
+  const [color, setColor] = useState("grey");
+  const initialWarning = { date: true, fabric: true, quantity: true };
+  const [warnings, setWarnings] = useState(initialWarning);
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    if (values.quantity < 100 && values.quantity > 1) {
+      setWarnings({ ...warnings, quantity: true });
+    } else {
+      setWarnings({ ...warnings, quantity: false });
+    }
+  }, [values.quantity]);
+
+  useEffect(() => {
+    if (values.fabric) {
+      setWarnings({ ...warnings, fabric: true });
+      setColor("black");
+    } else {
+      setWarnings({ ...warnings, fabric: false });
+    }
+  }, [values.fabric]);
+
+  useEffect(() => {
+    let dateObj = new Date(values.date);
+    let todayObj = new Date();
+    if (dateObj <= todayObj && values.date !== "") {
+      setWarnings({ ...warnings, date: false });
+      console.log(values.date);
+    } else {
+      setWarnings({ ...warnings, date: true });
+    }
+  }, [values.date]);
+
+  const typeHandler = () => {
+    setType("date");
+  };
+
   return (
     <div className="App">
-      <Calculator/>
+      <div className="calc">
+        <div>
+          <div className="company">The Company</div>
+          <div className="shipping-time">
+            Shipping Time <br /> Calculator
+          </div>
+          <ul className="inputs">
+            <li className="input-li">
+              <div className="input-box">
+                <span className={warnings.date ? "closed warn" : "opened warn"}>
+                  Can't select a past date
+                  <TiWarningOutline className="warn-icon" />
+                </span>
+                <BsCalendar4 className="icon" />
+                <input
+                  placeholder="Order Date"
+                  className="input date"
+                  type={type}
+                  onFocus={typeHandler}
+                  onBlur={typeHandler}
+                  id="date"
+                  value={values.date}
+                  onChange={(e) =>
+                    setValues({ ...values, date: e.target.value })
+                  }
+                />
+              </div>
+            </li>
+            <li className="input-li">
+              <div className="input-box">
+                <span
+                  className={warnings.fabric ? "closed warn" : "opened warn"}
+                >
+                  Select a fabric type
+                  <TiWarningOutline className="warn-icon" />
+                </span>
+                <IoIosArrowDown className="icon" />
+                <select
+                  className={`input fabric ${color}`}
+                  type="radio"
+                  placeholder="Fabric Type"
+                  value={values.fabric}
+                  onChange={(e) =>
+                    setValues({ ...values, fabric: e.target.value })
+                  }
+                >
+                  <option className="option" value="" style={{ color: "grey" }}>
+                    Fabric Type
+                  </option>
+                  <option className="option" value="cotton">
+                    Cotton
+                  </option>
+                  <option className="option" value="linen">
+                    Linen
+                  </option>
+                </select>
+              </div>
+            </li>
+            <li className="input-li">
+              <div className="input-box">
+                {isShown ? (
+                  <span className="info">
+                    Shipping Dates May Vary Based on Quantity
+                  </span>
+                ) : (
+                  <div style={{ visibility: "hidden" }}></div>
+                )}
+
+                <span
+                  className={warnings.quantity ? "closed warn" : "opened warn"}
+                >
+                  Select between 1 and 100
+                  <TiWarningOutline className="warn-icon" />
+                </span>
+                <svg
+                  onMouseEnter={() => setIsShown(true)}
+                  onMouseLeave={() => setIsShown(false)}
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 1024 1024"
+                  className="icon info-icon"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                ></svg>
+                <input
+                  type="number"
+                  className="input quantity"
+                  placeholder="Quantity"
+                  value={values.quantity}
+                  onChange={(e) =>
+                    setValues({ ...values, quantity: Number(e.target.value) })
+                  }
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
+        <Calculate
+          props={values.quantity}
+          values={values}
+          warnings={warnings}
+          setWarnings={setWarnings}
+          setType={setType}
+          setColor={setColor}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/calculate/Calculate.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+
+const Calculator = ({ values,props}) => {
+  const [popup, setPopup] = useState(false);
+  const [calculated, setCalculated] = useState(false);
+  const [shippingDay, setShippingDay] = useState();
+
+  const options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+
+  const today = new Date().toLocaleString("tr-TR", options);
+
+  function isWeekend(date) {
+    return date.getDay() === 6 || date.getDay() === 0;
+  }
+
+  function isHoliday(date) {
+    const holidays = { "6,4": "holiday 1", "11,25": "holiday 2" };
+    const anyHoliday = Object.keys(holidays).some(
+      (el) => el === `${date.getMonth()},${date.getDate()}`
+    );
+    return anyHoliday;
+  }
+
+
+
+  const calculateHandler = () => {
+    const newDate = new Date(values.date).toLocaleString("tr-TR", options);
+    const orderDate = new Date(values.date);
+    let delay = 0;
+    if (
+      (newDate) =>
+        today && values.fabric && values.quantity < 100 && values.quantity > 1
+    ) {
+      let shippingProcess;
+      if (values.fabric === "cotton") {
+        if (values.quantity < 50) {
+          shippingProcess = 2 + delay;
+        } else {
+          shippingProcess = 3 + delay;
+        }
+      } else {
+        if (values.quantity < 50) {
+          shippingProcess = 4 + delay;
+        } else {
+          shippingProcess = 5 + delay;
+        }
+      }
+
+      console.log("SHIPPING PROCESS - " + shippingProcess);
+      let days = [orderDate];
+      console.log("DAYS BEFORE PUSHING - " + days);
+      for (let i = 1; i <= 10; i++) {
+        const dummyDate = new Date(values.date);
+        days.push(new Date(dummyDate.setDate(dummyDate.getDate() + i)));
+      }
+      let filteredDays = days.filter((date) => isWeekend(date) === false);
+      filteredDays = filteredDays.filter((date) => isHoliday(date) === false);
+      let finalDay = filteredDays[shippingProcess];
+      const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      };
+      setShippingDay(finalDay.toLocaleString("tr-TR", options));
+      setCalculated(true);
+    } else {
+      setPopup(true);
+    }
+  };
+
+  return (
+    <div>
+      <div className="button">
+        <button onClick={calculateHandler} className="calc-button">
+          Calculate
+        </button>
+        {!calculated ? (
+          <div className="estimated">
+            Please enter your order information to estimate shipping date
+          </div>
+        ) : (
+          <div className="estimated">
+            Your Estimated Shipping Time is <span>{shippingDay}</span>
+          </div>
+        )}
+      </div>
+      {popup ? (
+        <div className="popup">
+          <div className="popup_inner">
+            <h1 className="popup-text">
+              Please Fill All Information Correctly
+            </h1>
+            <button className="popup-button" onClick={() => setPopup(false)}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div style={{ visibility: "hidden" }}></div>
+      )}
+    </div>
+  );
+};
+
+export default Calculator;
